f34c057c65991248c91302be095f4cf2ac7382ae
IrynaKhonina <irinahonina@yahoo.com>
2025-01-05 18:04:08 +0300

    48

--- src/App.tsx
+++ src/App.tsx
@@ -81,14 +81,22 @@ export const Header = () => {
 
 // Компонент Menu
 export const Menu = () => {
+    // Если массив пуст, показываем сообщение
+    const numPizzas = pizzaData.length;
+
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <ul className="pizzas">
-                {pizzaData.map((pizza) => (
-                    <Pizza pizzaObj={pizza} key={pizza.name} />
-                ))}
-            </ul>
+
+            {numPizzas > 0 ? (
+                <ul className="pizzas">
+                    {pizzaData.map((pizza) => (
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                    ))}
+                </ul>
+            ) : (
+                <p>We're still working on our menu. Please come back later :)</p>
+            )}
         </main>
     );
 };
@@ -117,7 +125,10 @@ export const Footer = () => {
     return (
         <footer className="footer">
             {isOpen ? (
-                <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+                <div className="order">
+                    <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+                    <button className="btn">Order</button>
+                </div>
             ) : (
                 <p>We're closed. We open at {openHour}:00.</p>
             )}
